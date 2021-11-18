fd6167c6c9032f283da5303fb3bfd391ffad3f4a
FIRLANAGT <92157918+FIRLANAGT@users.noreply.github.com>
2021-11-18 15:52:35 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const server = express()
+
+server.all("/", (req, res) => {
+
+  res.send('<meta http-equiv="refresh" content="0; URL=https://phantomcodes.ga/support"/>')
+
+})
+
+function keepAlive() {
+
+  server.listen(3000, () => {
+
+    console.log("Server is ready.")
+
+  })
+
+}
+
+module.exports = keepAlive
